bb5e0580cab29e604581006370e81d0b89eb9a85
Shoaib Amjad <shoaibamjaddev@gmail.com>
2025-09-21 20:24:38 -0700

    Node JS Updated Code

--- Class 4 ExpressJS Routing/index.js
+++ Class 4 ExpressJS Routing/index.js
@@ -45,8 +45,29 @@ app.get('/about/:id', (req, res) => {
 
 
 //  abb iskoo thorraa complicated yaani subroutes yani nested route kayb sth banata hoon 
-// 
-app.get('/about/:id', (req, res) => {
+// iss main app ko kabhi space nhi lagani ha 
+// jaisay maa likh taa hoon about/ kay sath :userid and phirr maa aiik orr bana layta hoon jaisay :bookid 
+app.get('/about/:userid/book/:bookid', (req, res) => {
     res.send(req.params)/* abb iskoo print karnay kay bohut saaray methods hain jaisay manay likha ha req.params */
 })
-// iss ko run karnay kay liyay muj ko Localhost:3000 kay sath /about / agay phi koi bhi value bhejj nhi haa
+// iss main ap hali numeric value nhi likhtay balkay kuch bhi alphabets bhi numbers bhi etc.
+// iss ko run karnay kay liyay muj ko Localhost:3000 kay sath /about kay baad /user agay phi koi bhi value bhejj nhi haa phir agay /book agay phir koi value bhej nii ha
+
+
+// iss main koi zarroori nhi ha kay multiples route banaye balkay app iss main aik route main bhi multiple values send kar saktay hain 
+// uss kay liyay muj ko :userid kay sath - aur :bookid likhna ha 
+app.get('/about/:userid-:bookid', (req, res) => {
+    res.send(req.params)/* abb iskoo print karnay kay bohut saaray methods hain jaisay manay likha ha req.params */
+})
+// iss ko run karnay kay liyay muj ko Localhost:3000 kay sath /about kay baad /user agay phi koi bhi value bhejj nhi haa phir agay - lagana ha agay phir koi value bhej nii ha
+
+
+//   yahan par inn values koo send karnay kaa aik orr bhi method haa
+//  jaisaay maa Localhost:3000 kay sath search naam ka aik route ha uss kay sath maa laaga doo ga ?name= phi koi bhi value dal doo gaa
+// iss koo bananay kay liyay hum query params ka use kartay hain 
+app.get('/search', (req, res) => {
+    res.send(req.query)/* abb iskoo print karnay kay bohut saaray methods hain jaisay manay likha ha req.query likha haa */
+})
+// iss ko run karnay kay liyay muj ko Localhost:3000 kay sath /search kay sath ? mark dal  agay phi koi bhi value bhejj nhi haa 
+// humm iss maain aik say zayda hi value bhi send kar saktay hain 
+// agar apko iss main multiple query bhej nhi haa too apko har bar iskay sath & ka sign lagana paray ga
